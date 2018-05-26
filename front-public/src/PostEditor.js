1197f5584cdbef4ed04dc047a9da7d8b36528113
Benoît Hubert <benoithubert@gmail.com>
2018-05-27 00:04:54 +0200

    Parent component state update on child component event

--- front-public/src/PostEditor.js
+++ front-public/src/PostEditor.js
@@ -8,19 +8,29 @@ class PostEditor extends React.Component {
       content: ''
     }
     this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
   handleChange (event) {
     this.setState({
       [event.target.name]: event.target.value
     })
   }
+  handleSubmit (event) {
+    event.preventDefault()
+    this.props.addPost(this.state)
+    this.setState({
+      title: '',
+      content: ''
+    })
+  }
   render () {
     const { title, content } = this.state
     return (
-      <div>
+      <form onSubmit={ this.handleSubmit }>
         <input name="title" placeholder="Post title" value={ title } onChange={ this.handleChange } /><br />
-        <textarea name="content" placeholder="Post content" value={ content } onChange={ this.handleChange }></textarea>
-      </div>
+        <textarea name="content" placeholder="Post content" value={ content } onChange={ this.handleChange }></textarea><br />
+        <button type="submit">Publish</button>
+      </form>
     )
   }
 }
