9f8afed0478bd39b64a1a038d6ab987ffb89cb16
Benoît Hubert <benoithubert@gmail.com>
2018-05-26 22:19:40 +0200

    Demonstrate state with new PostEditor component

---
+++ front-public/src/PostEditor.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+class PostEditor extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      title: '',
+      content: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange (event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  render () {
+    const { title, content } = this.state
+    return (
+      <div>
+        <input name="title" placeholder="Post title" value={ title } onChange={ this.handleChange } /><br />
+        <textarea name="content" placeholder="Post content" value={ content } onChange={ this.handleChange }></textarea>
+      </div>
+    )
+  }
+}
+
+export default PostEditor
