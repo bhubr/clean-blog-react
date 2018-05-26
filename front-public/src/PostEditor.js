08752512d103b189a5d14f48bb5734e54f9f3e05
Benoît Hubert <benoithubert@gmail.com>
2018-05-26 22:16:40 +0200

    clean-up

--- front-public/src/PostEditor.js
+++ front-public/src/PostEditor.js
@@ -2,16 +2,14 @@ import React from 'react'
 
 class PostEditor extends React.Component {
   constructor (props) {
-    super (props)
+    super(props)
     this.state = {
       title: '',
       content: ''
     }
     this.handleChange = this.handleChange.bind(this)
   }
-  handleChange(event) {
-    console.dir(event.target)
-    console.log(event.target.name, event.target.value)
+  handleChange (event) {
     this.setState({
       [event.target.name]: event.target.value
     })
