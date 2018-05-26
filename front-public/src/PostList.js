b95f131da494f5b094c44188dd68790dbe0403f6
Benoît Hubert <benoithubert@gmail.com>
2018-05-26 23:57:09 +0200

    Parent component state update on child component event

--- front-public/src/PostList.js
+++ front-public/src/PostList.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Post from './Post'
 
-import posts from './posts'
-
 class PostList extends React.Component {
   render () {
     return (
       <div>
         {
-          posts.map((post, i) => <Post key={i} title={ post.title } content={ post.content } />)
+          this.props.posts.map((post, i) => <Post key={i} title={ post.title } content={ post.content } />)
         }
       </div>
     )
