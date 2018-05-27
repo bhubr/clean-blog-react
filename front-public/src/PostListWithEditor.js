adc426766ddb80c591d3278711198d50941cb56c
Benoît Hubert <benoithubert@gmail.com>
2018-05-27 13:06:57 +0200

    Add basic routing with React Router

---
+++ front-public/src/PostListWithEditor.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import PostList from './PostList'
+import PostEditor from './PostEditor'
+
+import posts from './posts'
+
+class PostListWithEditor extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      posts
+    }
+    this.addPost = this.addPost.bind(this)
+  }
+  addPost (postData) {
+    const { posts } = this.state
+    // Use "spread operator" to make a copy of the posts array,
+    // instead of modifying it directly
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+    const newPosts = [ ...posts ]
+    newPosts.push(postData)
+    // Pass the newly created array as value for state's posts
+    this.setState({
+      posts: newPosts
+    })
+  }
+  render () {
+    return (
+      <div className="App">
+        <PostList posts={this.state.posts} />
+        <PostEditor addPost={ this.addPost } />
+      </div>
+    )
+  }
+}
+
+export default PostListWithEditor
